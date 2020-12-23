b247382a3f327ec4ab11edc9a00bc18fd65445c6
shivaguru2007-coder <65496645+shivaguru2007-coder@users.noreply.github.com>
2020-12-23 18:53:44 +0530

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,29 @@
+class Drops{
+    constructor(x, y) {
+      var options = {
+          restitution:0.1,
+          friction:0.001,
+          //density:1.2,
+          isStatic:false
+          
+
+      }
+      this.body = Bodies.circle(x,y,5, options);
+      this.radius=5
+      World.add(world, this.body);
+    }
+
+    updateY(){
+      if (this.body.position.y > height){
+        Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400)})
+      }
+
+    }
+
+    showDrop(){
+      fill("blue");
+      ellipseMode(CENTER);
+        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+    }
+  }
+ 
